994fe9a7ba330d4df3dee052a675efd1fc8480be
winsis <maciassecret@gmail.com>
2021-10-18 16:37:47 -0500

    Se agrega la clase information a models

--- user_interface/models.py
+++ user_interface/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.contrib.auth.models import AbstractUser
+from django.db.models.fields import CharField
 
 # Create your models here.
 class User(AbstractUser):
@@ -7,3 +8,21 @@ class User(AbstractUser):
 
 class information(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
+    fullName = models.CharField(max_length=50, blank=True, null=True)
+    bio = models.CharField(max_length=500, blank=True, null=True)
+    about = models.TextField(blank=True, null=True)
+    address = models.CharField(max_length=100, blank=True, null=True)
+    email = models.EmailField(max_length=100, blank=True, null= True)
+    phone = models.CharField(max_length=15, blank=True, null=True)
+    avatar = models.ImageField(upload_to="avatar/", blank=True, null=True)
+    CV =    models.FileField(upload_to="cv/", blank=True, null=True)
+
+    # Social Networks
+    github = models.URLField(blank=True, null=True)
+    facebook = models.URLField(blank=True, null=True)
+    instagram = models.URLField(blank=True, null=True)
+    linkedin = models.URLField(blank=True, null=True)
+    other = models.URLField(blank=True, null=True)
+    
+
+
